a63bf4aa1d57debaaa635701521d69a0f0f69521
Sreyas P Pradeep <sreyasinemily@gmail.com>
2025-06-27 21:18:48 +0530

    added getUsersForSidebar, message model, and misc

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.route.js';
+import messageRoute from './routes/message.route.js';
 import { connectDB } from './lib/db.js';
 import cookieParser from 'cookie-parser';
 dotenv.config();
@@ -8,7 +9,9 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cookieParser())
+
 app.use('/api/auth', authRoutes);
+app.use('api/message',messageRoute);
 
 app.listen(process.env.PORT, () => {
     console.log("Server is running on port " + process.env.PORT);
