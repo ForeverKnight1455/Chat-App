c69444f686e3f53856f0ac66dcefe6dd8f8a5262
Sreyas P Pradeep <sreyasinemily@gmail.com>
2025-06-27 23:52:51 +0530

    included functionalities in message routes and controller

--- backend/src/controllers/messsage.controller.js
+++ backend/src/controllers/messsage.controller.js
@@ -1,6 +1,6 @@
 import Message from '../models/message.model.js';
 import User from '../models/user.model.js';
-
+import cloudinary from '../lib/cloudinary.js';
 export const getUsersForSidebar = async(req,res) => {
     try{
         const loggedInUserId = req.user._id;
@@ -23,5 +23,37 @@ export const getMessages = async (req,res) => {
                 {senderId:userToChatId, receiverId:myId}
             ]
         })
+    }
+    catch(error){
+        console.error("error in getMessages controller:", error.message);
+        res.status(500).json({error:"internal server error"});
+    }
+}
+
+export const sendMessage = async (req,res) => {
+    try {
+        const {text,image} = req.body;
+        const {id:receiverId} = req.params;
+        const senderId = req.user._id;
+        let imageurl;
+        if(image){
+            const uploadResponse =  await cloudinary.uploader.upload(image);
+           imageurl = uploadResponse.secure_url;
+        }
+
+        const newMessage = new Message({
+            senderId,
+            receiverId,
+            text,
+            image:imageurl,
+        });
+
+        await newMessage.save()
+        //todo: realtime functionality using socket.io 
+        res.status(200).json(newMessage);
+    }
+    catch(error){
+        console.error("error in sendMessage controller:", error.message);
+        res.status(500).json({error:"internal server error"});
     }
 }
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import { protectRoute } from '../middleware/auth.middleware';
-import { getUsersForSidebar,getMessages } from '../controllers/messsage.controller';
+import { getUsersForSidebar,getMessages,sendMessage } from '../controllers/messsage.controller';
 const router = express.Router();
 
 router.get('/user',protectRoute, getUsersForSidebar);
 router.get('/:id',protectRoute,getMessages);
+
+router.post('/send/:id',protectRoute,sendMessage);
+
 export default router;
