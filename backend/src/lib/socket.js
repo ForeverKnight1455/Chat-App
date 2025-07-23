af03722dfb9f18332b1ef0bce2684c815973ecf3
Sreyas P Pradeep <sreyasinemily@gmail.com>
2025-07-23 22:48:56 +0530

    completed the socket programming

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -11,12 +11,28 @@ const io = new Server(server, {
     }
 })
 
+export function getReceiverSocketId(userId){
+    return userSocketMap[userId];
+}
+
+//online users
+const userSocketMap = {};
+
 io.on("connection", (socket) => {    
     
     console.log("user connected:", socket.id);
 
+    const userId = socket.handshake.query.userId;
+    if(userId){
+        userSocketMap[userId] = socket.id;
+    }
+
+    io.emit("getOnlineUsers",Object.keys(userSocketMap));
+
     socket.on("disconnect", () => {
-        console.log("a user disconnected: ", socket.id)
+        console.log("a user disconnected: ", socket.id);
+        delete userSocketMap[userId];
+        io.emit("getOnlineUsers",Object.keys(userSocketMap));
     });
 })
 
