8dd4b3510cf47f6bb8f61f9781fa3ddff6a4679d
Sreyas P Pradeep <sreyasinemily@gmail.com>
2025-07-23 23:01:27 +0530

    create dist folder and made modification in the mode

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,6 +6,8 @@ import { connectDB } from './lib/db.js';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import {app,server} from './lib/socket.js'
+import path from "path";
+
 dotenv.config();
 
 app.use(express.json());
@@ -18,6 +20,15 @@ app.use(cors({
 
 app.use('/api/auth', authRoutes);
 app.use('/api/message',messageRoute);
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+
+    app.get("*",(req,res) => {
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+    })
+}
 
 server.listen(process.env.PORT, () => {
     console.log("Server is running on port " + process.env.PORT);
